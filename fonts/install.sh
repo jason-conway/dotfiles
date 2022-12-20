08d364ea0602c018431afb9b3a774176fdbf8186
Jason Conway <jpc@jasonconway.dev>
2022-12-20 17:47:02 -0600

    Organize

--- fonts/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-cp -r ./JetBrainsMono/ /usr/share/fonts/
-cp -r ./IBM-Plex-Mono/ /usr/share/fonts/
-
-fc-cache -f -v
--- gnome-terminal/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -e
-
-dconf load /org/gnome/terminal/legacy/profiles:/ < profiles.dconf
