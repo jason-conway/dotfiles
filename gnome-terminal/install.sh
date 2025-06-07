7d50e49a9a621d2090032f8966d62721a4058a8c
Jason Conway <jpc@jasonconway.dev>
2025-06-06 21:06:08 -0400

    Add update/install scripts for gnome-terminal config

---
+++ gnome-terminal/install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+dconf load /org/gnome/terminal/legacy/profiles:/ < profiles.dconf
---
+++ gnome-terminal/update.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+dconf dump /org/gnome/terminal/legacy/profiles:/ > profiles.dconf
