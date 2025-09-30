b6fb096f7f6a4b82442f259221b086900b68abe6
Jason Conway <jpc@jasonconway.dev>
2025-09-29 21:41:31 -0400

    [vscode] update scripts

--- vscode/install.sh
+++ vscode/install.sh
@@ -4,10 +4,18 @@ SRC="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 if [[ `uname` == "Darwin" ]]; then
     DST="$HOME/Library/Application Support/Code/User"
+    PLATFORM="mac"
 elif [[ `uname` == "Linux" ]]; then
     DST="$HOME/.config/Code/User"
+    PLATFORM="linux"
 fi
 
 if [ -d "$DST" ]; then
-    cp "$SRC/keybindings.json" "$SRC/settings.json" "$DST/"
+    if [ -f "$SRC/keybindings-$PLATFORM.json" ]; then
+        cp "$SRC/keybindings-$PLATFORM.json" "$DST/keybindings.json"
+    fi
+    
+    if [ -f "$SRC/settings-$PLATFORM.json" ]; then
+        cp "$SRC/settings-$PLATFORM.json" "$DST/settings.json"
+    fi
 fi
--- vscode/update.sh
+++ vscode/update.sh
@@ -2,13 +2,15 @@
 
 DST="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-
 if [[ `uname` == "Darwin" ]]; then
     SRC="$HOME/Library/Application Support/Code/User"
+    PLATFORM="mac"
 elif [[ `uname` == "Linux" ]]; then
     SRC="$HOME/.config/Code/User"
+    PLATFORM="linux"
 fi
 
 if [ -d "$SRC" ]; then
-    cp "$SRC/keybindings.json" "$SRC/settings.json" "$DST/"
+    cp "$SRC/keybindings.json" "$DST/keybindings-$PLATFORM.json"
+    cp "$SRC/settings.json" "$DST/settings-$PLATFORM.json"
 fi
